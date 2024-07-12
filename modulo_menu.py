9fc3fa37891dfc6aee22b5be0ccfb2ea4d18f6d6
AlexisH28 <alexismar1228@gmail.com>
2024-07-12 17:09:39 -0500

    feat: :sparkles: Se modificó el diseño
    @AlexisH28

--- modulo_menu.py
+++ modulo_menu.py
@@ -7,7 +7,6 @@ def menu_principal():
     print("***************************************************************")
     print("Acá encontraras múltiples opciones para el registro de ciudades")
     print("***************************************************************")
-    print()
     print("///////////////////////////////////////////////////////////////////")
     print("1. Agregar | 2. Editar | 3. Buscar | 4. Ver ciudades | 5. Finalizar")
     print("///////////////////////////////////////////////////////////////////")
