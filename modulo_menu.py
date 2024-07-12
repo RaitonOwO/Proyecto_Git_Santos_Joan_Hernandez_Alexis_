f9be8ca40c3465f4008e33e73523f15e4fe2ded3
RaitonOwO <jhoanowo@gmail.com>
2024-07-12 16:50:50 -0500

    feat: :sparkles: se creo el modulo de menus
    se creo el menu principal

---
+++ modulo_menu.py
@@ -0,0 +1,14 @@
+
+def menu_principal():
+    print("*************************************************")
+    print("                ¡ BIENVENIDOS !                  ")
+    print("*************************************************")
+    print("            ¿ En que podemos ayudarte ?          ")
+    print("***************************************************************")
+    print("Acá encontraras múltiples opciones para el registro de ciudades")
+    print("***************************************************************")
+    print()
+    print("///////////////////////////////////////////////////////////////////")
+    print("1. Agregar | 2. Editar | 3. Buscar | 4. Ver ciudades | 5. Finalizar")
+    print("///////////////////////////////////////////////////////////////////")
+
